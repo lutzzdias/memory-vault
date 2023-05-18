a1715c15f40e63dd88a49f8fbb5e73c2db640b23
lutzzdias <thiagolutzdias@hotmail.com>
2023-05-17 23:38:48 -0400

    feat(web): show user info in home screen if authenticated

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,18 +1,22 @@
+import { cookies } from 'next/headers'
+
 import { Blur } from '@/components/Blur'
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { SignIn } from '@/components/SignIn'
 import { Stripes } from '@/components/Stripes'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         <Blur />
         <Stripes />
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
