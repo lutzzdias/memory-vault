eff6320f337446e8f2b28e2d4faf52043e58fc1e
lutzzdias <thiagolutzdias@hotmail.com>
2023-05-17 23:30:09 -0400

    feat(web): implement oauth and cookie with backend signin route

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,22 @@
+import { api } from '@/lib/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  const registerResponse = await api.post('/signin', {
+    code,
+  })
+
+  const { token } = registerResponse.data
+
+  // save token in cookies and redirect user back to home
+  const redirectURL = new URL('/', request.url)
+  const cookieExpiresInSeconds = 7 * 24 * 60 * 60 // days * hours * minutes * seconds
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}`,
+    },
+  })
+}
