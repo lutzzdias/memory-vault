422171942074160220b108dc9bf0f2ce38167ac1
lutzzdias <thiagolutzdias@hotmail.com>
2023-05-17 22:35:09 -0400

    feat(api): create signin route

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,17 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // TODO: specify what addresses can access this api (dev, production, etc)
 })
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
