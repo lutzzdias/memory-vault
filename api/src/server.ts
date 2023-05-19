d5b2d2871a0544c92cffca2ab2503e486ef19a28
lutzzdias <thiagolutzdias@hotmail.com>
2023-05-19 13:50:59 -0400

    fix(api): comment host 0.0.0.0 for web to work

--- api/src/server.ts
+++ api/src/server.ts
@@ -21,7 +21,7 @@ app.register(authRoutes)
 app
   .listen({
     port: 3333,
-    host: '0.0.0.0',
+    //host: '0.0.0.0',
   })
   .then(() => {
     console.log('🔥 HTTP server running on http://localhost:3333')
