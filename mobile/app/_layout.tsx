51d8f91fba2ae484deecf36ae473924a07e4b995
lutzzdias <thiagolutzdias@hotmail.com>
2023-05-21 19:43:12 -0400

    feat(mobile): send user to correct page depending on authentication

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,4 +1,9 @@
+import { useEffect, useState } from 'react'
 import { ImageBackground } from 'react-native'
+import { SplashScreen, Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
+
 import { styled } from 'nativewind'
 
 import { useFonts, BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
@@ -6,17 +11,25 @@ import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import blurBg from '../src/assets/bg-blur.png'
 import Stripes from '../src/assets/stripes.svg'
-import { SplashScreen, Stack } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
 
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    boolean | null
+  >(null)
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
 
   if (!hasLoadedFonts) {
     return <SplashScreen />
@@ -36,7 +49,11 @@ export default function Layout() {
           headerShown: false,
           contentStyle: { backgroundColor: 'transparent' },
         }}
-      />
+      >
+        {/* redirect user to next route (memories) in case the redirect (isUserAuthenticated) is true */}
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
